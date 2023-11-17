e1774dc1a3946d9cdf2ef3098b029c002e2bef88
MGutix <140160200+MGutix@users.noreply.github.com>
2023-11-17 10:05:25 +0900

    Create tests for first 3 exercises

--- sum.js
+++ sum.js
@@ -1,4 +1,30 @@
-function sum(a, b) {
+export function sum(a, b) {
     return a + b;
   }
-  module.exports = sum;
+
+export function capitalize(string) {
+  let letterArray = string.split('')
+  letterArray[0] = letterArray[0].toUpperCase()
+  return letterArray.join('')
+}
+
+export function reverseString(string) {
+  return string.split('').reverse().join('')
+}
+
+export const calculator = {
+  add(num1, num2){
+    return num1 + num2
+  },
+  subtract(num1, num2){
+    return num1 - num2
+  },
+  divide(num1, num2){
+    return num1 / num2
+  },
+  multiply(num1, num2){
+    return num1 * num2
+  },
+}
+
+
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,28 @@
-const sum = require('./sum');
+import { calculator, sum, capitalize, reverseString } from './sum';
+
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('passes marcos to equal Marcos', () =>{
+  expect(capitalize('marcos')).toBe('Marcos')
+})
+
+test ('reverses bouldering', () => {
+  expect(reverseString('bouldering')).toBe('gniredluob')
+})
+
+test('1 + 4', () =>{
+  expect(calculator.add(1,4)).toBe(5)
+})
+test('1 - 4', () =>{
+  expect(calculator.subtract(1,4)).toBe(-3)
+})
+test('1 / 4', () =>{
+  expect(calculator.divide(1,4)).toBe(0.25)
+})
+test('1 * 4', () =>{
+  expect(calculator.multiply(1,4)).toBe(4)
+})
+
